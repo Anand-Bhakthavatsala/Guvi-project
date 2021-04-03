3c93e2a660acbac163b80a12ce4409c5250f2c1c
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-04-03 22:49:53 +0530

    staging commit

---
+++ src/JavaProgram/Demo2.java
@@ -0,0 +1,21 @@
+package JavaProgram;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("First Java program");
+		
+		int i = 200; //4 bytes
+		long l = 4352342; //8 bytes 
+		double d = 23432.32432;
+		short s = 123;//2 bytes
+		char c = 'j';
+		boolean b = false;
+		
+		System.out.println("Value of integer i : "+ i);
+		
+	
+	}
+
+}
